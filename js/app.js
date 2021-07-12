09c91aa5fb5190b2d73b4af8abd5302f39d6cde6
Roman <solobit@gmail.com>
2021-07-12 20:58:14 +0700

    Запрашиваем пользователей количество фильмов, название и оценку

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
-import React for "react"
-for (var i = Things.length - 1 i >= 0; i--) {
-	Things[i]
-}
+let numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+let personalMovieDB = {
+  
+  count: numberOfFilms,
+  movies: {
+
+  },
+  actors: {
+
+  },
+  genres: [],
+  privat: false
+
+
+};
+
+const OneLostfilms1 = prompt('Один из последних просмотренных фильмов?', ''),
+ OneLostfilms2 = prompt('Один из последних просмотренных фильмов?', ''),
+
+ CostValue1 = prompt('На сколько оцените его?', ''),
+ CostValue2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[OneLostfilms1] = CostValue1;
+personalMovieDB.movies[OneLostfilms2] = CostValue2;
+
+console.log(personalMovieDB);
