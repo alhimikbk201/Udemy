84ac56c3ea1910799ebae8936da6cab189b7f05c
Roman <solobit@gmail.com>
2021-07-13 21:37:00 +0700

    Автоматизировали запросы при помощи цикла for проверили валидность вводимых данных

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-let numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+'use strict';
+let numberOfFilms =''; 
+
+  
+  //numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
+while(numberOfFilms ==='' || numberOfFilms === null || numberOfFilms.length > 50) {
+  numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
+}; 
+
+
 
 let personalMovieDB = {
   
@@ -15,13 +24,29 @@ let personalMovieDB = {
 
 };
 
-const OneLostfilms1 = prompt('Один из последних просмотренных фильмов?', ''),
- OneLostfilms2 = prompt('Один из последних просмотренных фильмов?', ''),
+for(let i = 0; i < 2; i++) {
+  
+  const OneLostfilms1 = prompt('Один из последних просмотренных фильмов?', '');
+  const CostValue1 = prompt('На сколько оцените его?', '');
+  
+  if(OneLostfilms1 !== null && OneLostfilms1 !== '' && OneLostfilms1.length < 50 && CostValue1 !== null && CostValue1 !=='' && CostValue1.length < 50) {
+    personalMovieDB.movies[OneLostfilms1] = CostValue1;
+    console.log('yes')
+} else {
+    console.log('error')
+    i--;
+}
+};
 
- CostValue1 = prompt('На сколько оцените его?', ''),
- CostValue2 = prompt('На сколько оцените его?', '');
+if(personalMovieDB.count < 10) {
+  alert("Просмотренно довольно мало фильмов");
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   alert("Вы классический зритель");
+} else if(personalMovieDB.count >= 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
 
-personalMovieDB.movies[OneLostfilms1] = CostValue1;
-personalMovieDB.movies[OneLostfilms2] = CostValue2;
 
 console.log(personalMovieDB);
