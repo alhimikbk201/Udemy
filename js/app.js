0a7dc16fa3c1178797e47136834c039b5895426d
Roman <solobit@gmail.com>
2021-07-15 18:52:15 +0700

    создали функции заполнения массива genres и функцию проверки базы данных на приватность

--- js/app.js
+++ js/app.js
@@ -25,30 +25,57 @@ let personalMovieDB = {
 
 
 };
-
-for(let i = 0; i < 2; i++) {
-  
-  const OneLostfilms1 = prompt('Один из последних просмотренных фильмов?', '');
-  const CostValue1 = prompt('На сколько оцените его?', '');
-  
-  if(OneLostfilms1 !== null && OneLostfilms1 !== '' && OneLostfilms1.length < 50 && CostValue1 !== null && CostValue1 !=='' && CostValue1.length < 50) {
-    personalMovieDB.movies[OneLostfilms1] = CostValue1;
-    console.log('yes')
-} else {
-    console.log('error')
-    i--;
-}
+function showMyDB() {
+  if(personalMovieDB.privat == false) {
+    console.log(personalMovieDB);
+  } else {
+    console.log("значение true");
+  }
 };
 
-if(personalMovieDB.count < 10) {
-  alert("Просмотренно довольно мало фильмов");
-} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-   alert("Вы классический зритель");
-} else if(personalMovieDB.count >= 30) {
-  alert("Вы киноман");
-} else {
-  alert("Произошла ошибка");
-}
+function writeYourGenres() {
+  
+  for(let i = 1; i < 4; i++) {
+    
+    personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр ${i}`);
+    console.log(personalMovieDB);
+  }
+};
+
+writeYourGenres();
+
+showMyDB();
+function rememberMyFilms() {
+  for(let i = 0; i < 2; i++) {
+  
+    const OneLostfilms1 = prompt('Один из последних просмотренных фильмов?', '');
+    const CostValue1 = prompt('На сколько оцените его?', '');
+    
+    if(OneLostfilms1 !== null && OneLostfilms1 !== '' && OneLostfilms1.length < 50 && CostValue1 !== null && CostValue1 !=='' && CostValue1.length < 50) {
+      personalMovieDB.movies[OneLostfilms1] = CostValue1;
+      console.log('yes')
+  } else {
+      console.log('error')
+      i--;
+  }
+  };
+};
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if(personalMovieDB.count < 10) {
+    alert("Просмотренно довольно мало фильмов");
+  } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+     alert("Вы классический зритель");
+  } else if(personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+  } else {
+    alert("Произошла ошибка");
+  }
+};
+
+detectPersonalLevel();
 
 
 console.log(personalMovieDB);
